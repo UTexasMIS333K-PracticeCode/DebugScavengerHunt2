7ba9f0d8c28b641f7449bdee6b0a6e5716e525e5
Muhammad Jawad <muhammad.jawad@mccombs.utexas.edu>
2024-04-02 23:17:06 -0500

    Add project files.

---
+++ DebugScavengerHunt2/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DebugScavengerHunt2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return the home page with the links to the other pages
+            return View();
+        }
+    }
+}
